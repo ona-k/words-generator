28c450be5ea87407bd8c9f2a7a1f810e86285509
alexander <agarber@profectusgroup.com>
2020-05-04 19:43:05 +1000

    column order

--- words-generator.rb
+++ words-generator.rb
@@ -13,7 +13,7 @@ class WordsGenerator
   end
 
   def noun_adjective
-    labels = %w[Skaičius Įvardžiuotinės Laipsnis Klausimas Būdvardis Daiktavardis]
+    labels = %w[Skaičius Klausimas Būdvardis Daiktavardis Įvardžiuotinės Laipsnis]
     number = []
     pronominality = []
     comparison = []
@@ -22,8 +22,8 @@ class WordsGenerator
     adjectives = []
 
     10.times { number << @number.sample.strip }
-    10.times { pronominality << @pronominality.sample.strip}
-    10.times { comparison << @comparison.sample.strip}
+    10.times { pronominality << @pronominality.sample.strip }
+    10.times { comparison << @comparison.sample.strip }
     10.times { interrogative_pronouns << @interrogative_pronouns.sample.strip }
     10.times { adjectives << @adjectives.sample.strip }
     10.times { nouns << @nouns.sample.strip }
@@ -32,13 +32,13 @@ class WordsGenerator
       labels,
       [
         number,
-        pronominality,
-        comparison,
         interrogative_pronouns,
         adjectives,
-        nouns
-        ]
-      )
+        nouns,
+        pronominality,
+        comparison
+      ]
+    )
     # MarkdownTables.plain_text(table)
   end
 end
@@ -50,7 +50,7 @@ adjectives_path = "#{source_path}/adjectives.txt"
 
 kwargs = {
   nouns: nouns_path,
-  adjectives: adjectives_path,
+  adjectives: adjectives_path
 }
 
 m = WordsGenerator.new(kwargs)
