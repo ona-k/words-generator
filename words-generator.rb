0348dab675d2f09ced2695520988267e5fff0dfd
alexander <agarber@profectusgroup.com>
2020-05-05 22:49:30 +1000

    pronominal

--- words-generator.rb
+++ words-generator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'markdown-tables'
+require 'redcarpet'
 
 class WordsGenerator
   def initialize(**kwargs)
@@ -12,12 +13,24 @@ class WordsGenerator
     @adjectives = File.readlines(kwargs[:adjectives])
   end
 
+  def add_commas(num_string)
+    num_string.reverse.scan(/\d{3}|.+/).join(',').reverse
+  end
+
+  def pronominate_adjective(str)
+    suffix = str[-2..-1]
+    mutable = %w[as us].include?(suffix)
+    pronominal = str + 'is'
+    mutable ? pronominal : str
+  end
+
   def noun_adjective
-    labels = %w[Skaičius Klausimas Būdvardis Daiktavardis Įvardžiuotinės Laipsnis]
+    labels = %w[Klausimas Numeris Būdvardis Daiktavardis Laipsnis]
     number = []
     pronominality = []
     comparison = []
     interrogative_pronouns = []
+    numerals = []
     nouns = []
     adjectives = []
 
@@ -28,20 +41,56 @@ class WordsGenerator
     10.times { adjectives << @adjectives.sample.strip }
     10.times { nouns << @nouns.sample.strip }
 
+    number.each do |n|
+      numerals << 1 if n.eql?('vienaskaita')
+      if n.eql?('daugiskaita')
+        num_str = (2..1_000_000).to_a.sample.to_s
+        numerals << add_commas(num_str)
+      end
+    end
+
+    pronominality.each_with_index do |n, i|
+      if n.eql?('įvardžiuotinė')
+        adj = pronominate_adjective(adjectives[i])
+        adjectives[i] = adj
+      end
+    end
+
     table = MarkdownTables.make_table(
       labels,
       [
-        number,
         interrogative_pronouns,
+        numerals,
         adjectives,
         nouns,
-        pronominality,
         comparison
       ]
     )
-    # MarkdownTables.plain_text(table)
   end
 end
+
+# Singular	Plural
+# Masculine	Feminine	Masculine	Feminine
+# N.	-asis	-oji	-ieji	-osios
+# G.	-ojo	-osios	-ųjų	-ųjų
+# D.	-ajam	-ajai	-iesiems	-osioms
+# A.	-ąjį	-ąją	-uosius	-ąsias
+# I.	-uoju	-ąja	-aisiais	-osiomis
+# L.	-ajame	-ojoje	-uosiuose	-osiose
+# V.	-asis!	-oji!	-ieji!	-osios!
+# Examples: baltasis, baltoji (the white one); žaliasis, žalioji (the green one); raudonasis, raudonoji (the red one); sausasis, sausoji (the dry one).
+
+# Nominal adjectives derived of the second declension:
+
+# Singular	Plural
+# Masculine	Feminine	Masculine	Feminine
+# N.	-usis	-ioji	-ieji	-iosios
+# G.	-iojo	-iosios	-iųjų	-iųjų
+# D.	-iajam	-iajai	-iesiems	-iosioms
+# A.	-ųjį	-iąją	-iuosius	-iąsias
+# I.	-iuoju	-iąja	-iaisiais	-iosiomis
+# L.	-iajame	-iojoje	-iuosiuose	-iosiose
+# V.	-usis!	-ioji!	-ieji!	-iosios!
 
 source_path = "#{Dir.home}/Development/words-generator/word_lists"
 output_path = "#{source_path}/words_table.md"
